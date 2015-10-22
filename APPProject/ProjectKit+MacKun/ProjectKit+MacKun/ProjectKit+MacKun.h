656c60596a45f3a06bccdcdfb27b1e0a15bea9e8
azheng51714 <414294494@qq.com>
2015-10-22 10:17:53 +0800

    ProjectKit

---
+++ APPProject/ProjectKit+MacKun/ProjectKit+MacKun/ProjectKit+MacKun.h
@@ -0,0 +1,19 @@
+//
+//  ProjectKit+MacKun.h
+//  ProjectKit+MacKun
+//
+//  Created by MacKun on 15/10/22.
+//  Copyright © 2015年 MacKun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProjectKit+MacKun.
+FOUNDATION_EXPORT double ProjectKit_MacKunVersionNumber;
+
+//! Project version string for ProjectKit+MacKun.
+FOUNDATION_EXPORT const unsigned char ProjectKit_MacKunVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProjectKit_MacKun/PublicHeader.h>
+
+
